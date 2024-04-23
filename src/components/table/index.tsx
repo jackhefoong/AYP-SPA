c5041dcbbbcdae9d674592505f26b9aa0a9812b2
Jackhe <jackhefoong@gmail.com>
2024-04-24 00:02:22 +0800

    Handle pagination

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Modal } from "../modal";
+import { Pagination } from "./pagination";
 
 export type Data = {
   id: number;
@@ -11,10 +13,14 @@ interface TableProps {
   data: Data[];
 }
 
+const DATA_PER_PAGE = 3;
 const HEADER_STYLING = "px-8 py-2 font-medium text-start";
 const DATA_STYLING = "px-8 py-2 h-11 max-w-xl truncate";
 
 export const Table: React.FC<TableProps> = ({ data }) => {
+  const TOTAL_PAGES = Math.ceil(data.length / DATA_PER_PAGE);
+  const [currentPage, setCurrentPage] = useState(1);
+
   const handleSave = (data: Data) => {
     // data[1].name = "hehe"; //doesn't update the json
     console.log(data);
@@ -37,26 +43,28 @@ export const Table: React.FC<TableProps> = ({ data }) => {
   const renderTableData = () => {
     return (
       <tbody>
-        {data.map((d, i) => {
-          return (
-            <tr
-              key={d.id}
-              className={`text-sm ${
-                i % 2 === 0 ? "bg-slate-200" : "bg-slate-50"
-              }`}
-            >
-              <td className={DATA_STYLING}>{d.id}</td>
-              <td className={DATA_STYLING}>{d.name}</td>
-              <td className={DATA_STYLING}>{d.email}</td>
-              <td className={DATA_STYLING}>
-                {d.isActive ? "ACTIVE" : "DEACTIVATED"}
-              </td>
-              <td className={DATA_STYLING}>
-                {d.isActive ? <Modal data={d} onSave={handleSave} /> : null}
-              </td>
-            </tr>
-          );
-        })}
+        {data
+          .slice((currentPage - 1) * DATA_PER_PAGE, currentPage * DATA_PER_PAGE)
+          .map((d, i) => {
+            return (
+              <tr
+                key={d.id}
+                className={`text-sm ${
+                  i % 2 === 0 ? "bg-slate-200" : "bg-slate-50"
+                }`}
+              >
+                <td className={DATA_STYLING}>{d.id}</td>
+                <td className={DATA_STYLING}>{d.name}</td>
+                <td className={DATA_STYLING}>{d.email}</td>
+                <td className={DATA_STYLING}>
+                  {d.isActive ? "ACTIVE" : "DEACTIVATED"}
+                </td>
+                <td className={DATA_STYLING}>
+                  {d.isActive ? <Modal data={d} onSave={handleSave} /> : null}
+                </td>
+              </tr>
+            );
+          })}
       </tbody>
     );
   };
@@ -67,6 +75,13 @@ export const Table: React.FC<TableProps> = ({ data }) => {
         {renderTableHeader()}
         {renderTableData()}
       </table>
+      <Pagination
+        data={{
+          currentPage: currentPage,
+          totalPages: TOTAL_PAGES,
+        }}
+        onPageChange={setCurrentPage}
+      />
     </div>
   );
 };
---
+++ src/components/table/pagination.tsx
@@ -0,0 +1,40 @@
+import { Label } from "../label";
+
+interface PaginationProps {
+  data: {
+    currentPage: number;
+    totalPages: number;
+  };
+  onPageChange: (v: number) => void;
+}
+
+export const Pagination: React.FC<PaginationProps> = ({
+  data,
+  onPageChange,
+}) => {
+  const handleBack = () => {
+    if (data.currentPage === 1) {
+      return;
+    }
+    onPageChange(data.currentPage - 1);
+  };
+  const handleForward = () => {
+    if (data.currentPage === data.totalPages) {
+      return;
+    }
+    onPageChange(data.currentPage + 1);
+  };
+  return (
+    <div className="flex gap-4 justify-end">
+      <button
+        onClick={handleBack}
+        disabled={data.currentPage === 1}
+      >{`<`}</button>
+      <Label label={data.currentPage.toString()} />
+      <button
+        onClick={handleForward}
+        disabled={data.currentPage === data.totalPages}
+      >{`>`}</button>
+    </div>
+  );
+};
